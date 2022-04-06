c1efa9ee5018a8194d6cea9696ac04ff6db8626b
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2022-04-06 10:39:09 +0500

    Drawing food

--- SnackGame/snackG.py
+++ SnackGame/snackG.py
@@ -66,6 +66,22 @@ class SnakeGameClass:
                 print(self.score)
 
 
+            # Draw Snack
+            if self.points:
+                for i, point in enumerate(self.points):
+                    if i != 0:
+                        cv2.line(imgMain, self.points[i - 1], self.points[i], (0, 0, 255), 20)
+                cv2.circle(imgMain, self.points[-1], 20, (200, 0, 200), cv2.FILLED)
+
+
+            # Draw food
+            rx, ry = self.foodPoint
+            imgMain = cvzone.overlayPNG(imgMain, self.imgFood,(rx - self.wFood // 2, ry - self.hFood // 2))
+                        
+
+
+
+
             
 
 

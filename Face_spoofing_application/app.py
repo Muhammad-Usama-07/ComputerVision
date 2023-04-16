fc235e52fc8a4fa81a3cd4007810a5973c7026fa
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2023-04-16 23:11:58 +0500

    saving currect picture on clicking on a button

--- Face_spoofing_application/app.py
+++ Face_spoofing_application/app.py
@@ -1,14 +1,18 @@
-from flask import Flask, jsonify, render_template
+import base64
+from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/data')
-def get_data():
-    data = {'key': 'value'}
-    return jsonify(data)
+@app.route('/save-image', methods=['POST'])
+def save_image():
+    data_url = request.json['dataURL']
+    image_data = base64.b64decode(data_url.split(',')[1])
+    with open('snapshot.png', 'wb') as f:
+        f.write(image_data)
+    return jsonify({'success': True})
 
 if __name__ == '__main__':
     app.run()

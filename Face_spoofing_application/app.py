677e71d351827c29c87f9c9876c6a6259f8f5e6d
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2023-05-16 18:32:24 +0500

    adding trained model, and predicting face spoofing

--- Face_spoofing_application/app.py
+++ Face_spoofing_application/app.py
@@ -2,7 +2,19 @@ import base64
 import cv2
 import face_recognition
 from flask import Flask, jsonify, render_template, request
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing.image import img_to_array
+import numpy as np
+
 app = Flask(__name__)
+
+json_file = open('./antispoofing_models/model.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+# load antispoofing model weights 
+model.load_weights('./antispoofing_models/model_weights.h5')
+print("Model loaded from disk")
 
 @app.route('/')
 def index():
@@ -23,6 +35,18 @@ def save_image():
     print(face_locations)
     for (top, right, bottom, left) in face_locations:
         cropped_face = image[top:bottom, left:right]
+        resized_face = cv2.resize(cropped_face,(160,160))
+        resized_face = resized_face.astype("float") / 255.0
+        resized_face = img_to_array(resized_face)
+        resized_face = np.expand_dims(resized_face, axis=0)
+        preds = model.predict(resized_face)[0]
+        print(preds)
+        if preds> 0.5:
+            label = 'spoof'
+            print(label)
+        else:
+            label = 'real'
+            print(label)
     cv2.imwrite('cropped_face.png', cv2.cvtColor(cropped_face, cv2.COLOR_RGB2BGR))
     return jsonify({'success': True, 'faces': len(face_locations)})
 

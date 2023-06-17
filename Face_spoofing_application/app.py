d9711fbe4800966e46564aa8f4db66884ca6894f
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2023-06-17 23:48:18 +0500

    Checking Real and Spoof face

--- Face_spoofing_application/app.py
+++ Face_spoofing_application/app.py
@@ -30,25 +30,35 @@ def save_image():
     # Load the saved image using face_recognition
     image = face_recognition.load_image_file('snapshot.png')
     # Find all the faces in the image
-    face_locations = face_recognition.face_locations(image)
-    # Return the number of faces found
-    print(face_locations)
-    for (top, right, bottom, left) in face_locations:
-        cropped_face = image[top:bottom, left:right]
-        resized_face = cv2.resize(cropped_face,(160,160))
-        resized_face = resized_face.astype("float") / 255.0
-        resized_face = img_to_array(resized_face)
-        resized_face = np.expand_dims(resized_face, axis=0)
-        preds = model.predict(resized_face)[0]
-        print(preds)
-        if preds> 0.5:
-            label = 'spoof'
-            print(label)
+    try: 
+        face_locations = face_recognition.face_locations(image)
+        print('face_locations: ', face_locations)
+        if face_locations == []:
+            print('Face not found----')
+            return jsonify({'success': False, 'face_match': 'Face Not Found'})
+        # Return the number of faces found
         else:
-            label = 'real'
-            print(label)
-    cv2.imwrite('cropped_face.png', cv2.cvtColor(cropped_face, cv2.COLOR_RGB2BGR))
-    return jsonify({'success': True, 'faces': len(face_locations)})
+            print(face_locations)
+            for (top, right, bottom, left) in face_locations:
+                cropped_face = image[top:bottom, left:right]
+                resized_face = cv2.resize(cropped_face,(160,160))
+                resized_face = resized_face.astype("float") / 255.0
+                resized_face = img_to_array(resized_face)
+                resized_face = np.expand_dims(resized_face, axis=0)
+                preds = model.predict(resized_face)[0]
+                print(preds)
+                if preds> 0.5:
+                    label = 'spoof'
+                    print(label)
+                else:
+                    label = 'real'
+                    print(label)
+            cv2.imwrite('cropped_face.png', cv2.cvtColor(cropped_face, cv2.COLOR_RGB2BGR))
+            return jsonify({'success': True, 'face_match': label})
+    except Exception as e:
+        return jsonify({'success': False, 'error': e})
+
+        
 
 if __name__ == '__main__':
     app.run()
